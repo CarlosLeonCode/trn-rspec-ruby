5aae854c7d877bb9055f988e0a25638fff863441
Carlos Leon <carlosestebanleonpinilla@gmail.com>
2022-05-14 23:58:50 -0500

    first spec

---
+++ spec/card_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../models/card.rb'
+
+RSpec.describe Card do
+  before do 
+    card = Card.new('Ace', 'Spades')
+  end
+
+  it 'has a rank' do
+    expect(card.rank).to eq('Ace')
+  end
+
+  it 'has a rank' do
+    expect(card.suit).to eq('Spades')
+  end
+end
